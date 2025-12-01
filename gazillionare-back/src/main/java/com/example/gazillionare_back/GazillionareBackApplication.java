8df6425a9b6f4741a4e4dbc1a42fb9e15e3ed35e
mcampos24 <mdcampos.iz2@gmail.com>
2025-12-01 11:38:25 -0500

    Initial commit

---
+++ gazillionare-back/src/main/java/com/example/gazillionare_back/GazillionareBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.gazillionare_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazillionareBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazillionareBackApplication.class, args);
+	}
+
+}
---
+++ gazillionare-back/src/test/java/com/example/gazillionare_back/GazillionareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gazillionare_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazillionareBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
